c357398e3d8212546c990aa577c9576a41c262dd
lbrande <lovbra00@gmail.com>
2021-03-19 02:38:02 +0100

    Added two example programs.

--- src/main/scala/examples/async.scala
+++ src/main/scala/examples/async.scala
@@ -5,6 +5,10 @@ import concurrent.Await.result
 import concurrent.Future
 import concurrent.duration.Duration
 import concurrent.ExecutionContext.Implicits.global
+import java.net._
+import java.net.http._
+import java.nio.file._
+import scala.jdk.CollectionConverters._
 
 @main def async = {
     trait Async[T] extends Eff {
@@ -23,7 +27,7 @@ import concurrent.ExecutionContext.Implicits.global
         def apply[T](fut: Future[T]): T / effect
     }
 
-    def async[R, E](prog: (async: Async_) ⇒ R / (async.effect & E)): R / E = handle { scope =>
+    def async[R, E](prog: (async: Async_) => R / (async.effect & E)): R / E = handle { scope =>
         val async = new Async_ {
             type effect = scope.effect
             def apply[T](t: =>T) = scope.switch { resume =>
@@ -32,7 +36,7 @@ import concurrent.ExecutionContext.Implicits.global
         prog(async)
     }
 
-    def await[R, E](prog: (await: Await_) ⇒ R / (await.effect & E)): R / E = handle { scope =>
+    def await[R, E](prog: (await: Await_) => R / (await.effect & E)): R / E = handle { scope =>
         val await = new Await_ {
             type effect = scope.effect
             def apply[T](fut: Future[T]) = scope.switch { resume =>
@@ -41,7 +45,7 @@ import concurrent.ExecutionContext.Implicits.global
         prog(await)
     }
 
-    def asyncMap[R, E, T](f: T => T, prog: (async: Async[T]) ⇒ R / (async.effect & E)): R / E = handle { scope =>
+    def asyncMap[R, E, T](f: T => T)(prog: (async: Async[T]) => R / (async.effect & E)): R / E = handle { scope =>
         val async = new Async[T] {
             type effect = scope.effect
             def apply(t: =>T) = scope.switch { resume =>
@@ -50,12 +54,71 @@ import concurrent.ExecutionContext.Implicits.global
         prog(async)
     }
 
-    def awaitMap[R, E, T](f: T => T, prog: (await: Await[T]) ⇒ R / (await.effect & E)): R / E = handle { scope =>
+    def awaitMap[R, E, T](f: T => T)(prog: (await: Await[T]) => R / (await.effect & E)): R / E = handle { scope =>
         val await = new Await[T] {
             type effect = scope.effect
             def apply(fut: Future[T]) = scope.switch { resume =>
                 resume(result(fut map f, Duration.Inf)) }
         }
         prog(await)
+    }
+
+    def readAll(async: Async[String], await: Await[String],
+                paths: List[Path]): String / (async.effect & await.effect) = paths match {
+        case head :: tail => for {
+            fut <- async { Files.readString(head) }
+            tail <- readAll(async, await, tail)
+            head <- await(fut)
+        } yield head + tail
+        case Nil => pure("")
+    }
+
+    {
+        val paths = Files.walk(Path.of("./src")).iterator.asScala.toList filter (!Files.isDirectory(_))
+        val keyword = "def"
+        val res = run {
+            asyncMap ((s: String) => (s.lines.iterator.asScala filter (_.contains(keyword))) mkString("\n")) { async =>
+                awaitMap (identity[String]) { await =>
+                    readAll(async, await, paths)
+                }
+            }
+        };
+
+        println(res);
+    }
+
+    def getAndWrite(async: Async_, await: Await_, 
+                    requests: List[(URI, Path)]): Unit / (async.effect & await.effect) = for {
+        fut <- getAndWriteHelper(async, await, requests)
+        _ <- await(fut)
+    } yield ()
+
+    def getAndWriteHelper(async: Async_, await: Await_, 
+                    requests: List[(URI, Path)]): Future[Unit] / (async.effect & await.effect) = requests match {
+        case (uri, path) :: tail =>
+            val request = HttpRequest.newBuilder.uri(uri).build
+            val handler =  HttpResponse.BodyHandlers.ofByteArray
+            for {
+                fut <- async { HttpClient.newHttpClient.send(request, handler) }
+                tail <- getAndWriteHelper(async, await, tail)
+                response <- await(fut)
+                fut <- async { Files.write(path, response.body) }
+            } yield fut flatMap (_ => tail)
+        case Nil => pure(Future.successful(()))
+    }
+
+    {
+        val uris = List.fill (8) { URI.create("https://www.google.com") }
+        val paths = List(Path.of("./a0"), Path.of("./a1"), Path.of("./a2"), Path.of("./a3"),
+                        Path.of("./a4"), Path.of("./a5"), Path.of("./a6"), Path.of("./a7"))
+        val res = run {
+            async { async =>
+                await { await =>
+                    getAndWrite(async, await, uris zip paths)
+                }
+            }
+        };
+
+        println(res);
     }
 }
